504efaf35b65e8310faa67959646ea82d4bbd564
Travis Keep <travis_keep@symantec.com>
2016-11-27 12:17:38 -0800

    Use goconvey for scotty/suggest test suite.

--- suggest/suggest_test.go
+++ suggest/suggest_test.go
@@ -2,88 +2,94 @@ package suggest_test
 
 import (
 	"github.com/Symantec/scotty/suggest"
-	"reflect"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
 func TestEngine(t *testing.T) {
-	engine := suggest.NewEngine()
-	assertValueEquals(t, 0, len(engine.Suggest(0, "")))
-	engine.Add("A")
-	engine.Add("Hi")
-	engine.Add("Hello")
-	engine.Add("Suggest")
-	engine.Add("A")
-	engine.Add("Hi")
-	engine.Add("Hello")
-	engine.Add("Suggest")
-	engine.Await()
-	assertValueDeepEquals(
-		t, []string{"Hello", "Hi"}, engine.Suggest(3, "H"))
-	assertValueDeepEquals(
-		t, []string{"Hello", "Hi"}, engine.Suggest(2, "H"))
-	assertValueDeepEquals(
-		t, []string{"Hello"}, engine.Suggest(1, "H"))
-	assertValueDeepEquals(
-		t, []string{"Hello", "Hi"}, engine.Suggest(0, "H"))
-	assertValueDeepEquals(
-		t, []string{"A", "Hello", "Hi", "Suggest"}, engine.Suggest(0, ""))
-	assertValueEquals(t, 0, len(engine.Suggest(0, "J")))
-	assertValueEquals(t, 0, len(engine.Suggest(5, "J")))
 
+	Convey("Given a new engine", t, func() {
+		engine := suggest.NewEngine()
+
+		Convey("No suggestions given initially", func() {
+			So(engine.Suggest(0, ""), ShouldBeEmpty)
+		})
+
+		Convey("After adding suggestions", func() {
+			engine.Add("Hello")
+			engine.Add("Suggest")
+			engine.Add("A")
+			engine.Add("Hi")
+			engine.Add("A")
+			engine.Add("Hi")
+			engine.Add("Hello")
+			engine.Add("Suggest")
+			engine.Await()
+
+			Convey("Suggestions should be in sorted order", func() {
+				So(
+					engine.Suggest(0, ""),
+					ShouldResemble,
+					[]string{"A", "Hello", "Hi", "Suggest"})
+			})
+
+			Convey("count = 0 gives all suggestions", func() {
+				So(
+					engine.Suggest(0, "H"),
+					ShouldResemble,
+					[]string{"Hello", "Hi"})
+			})
+
+			Convey("count > 0 gives at most count suggestions", func() {
+				So(
+					engine.Suggest(3, ""),
+					ShouldResemble,
+					[]string{"A", "Hello", "Hi"})
+			})
+
+			Convey("No match gives no suggestions", func() {
+				So(engine.Suggest(0, "J"), ShouldBeEmpty)
+			})
+
+		})
+
+	})
 }
 
-func TestConstEngine(t *testing.T) {
-	engine := suggest.NewSuggester(
-		"log", "logger", "loggest", "a", "an", "and", "aback")
-	assertValueDeepEquals(
-		t, []string{"log", "logger", "loggest"}, engine.Suggest(0, "log"))
-	assertValueDeepEquals(
-		t, []string{"log", "logger", "loggest"}, engine.Suggest(3, "log"))
-	assertValueDeepEquals(
-		t, []string{"log", "logger"}, engine.Suggest(2, "log"))
-	assertValueDeepEquals(
-		t, []string{"log"}, engine.Suggest(1, "log"))
-	assertValueDeepEquals(
-		t, []string{"logger", "loggest"}, engine.Suggest(2, "logg"))
-	assertValueDeepEquals(
-		t, []string{"a", "an", "and", "aback"}, engine.Suggest(0, "a"))
-	assertValueDeepEquals(
-		t, []string{"a", "an", "and", "aback"}, engine.Suggest(0, "a"))
-	assertValueDeepEquals(
-		t, []string{"aback"}, engine.Suggest(0, "ab"))
-	assertValueDeepEquals(
-		t, []string{"log", "logger"}, engine.Suggest(2, ""))
-	assertValueDeepEquals(
-		t, []string{"log", "logger", "loggest", "a", "an", "and", "aback"},
-		engine.Suggest(0, ""))
-	assertValueEquals(t, 0, len(engine.Suggest(0, "abc")))
-	assertValueEquals(t, 0, len(engine.Suggest(0, "m")))
-	assertValueEquals(t, 0, len(engine.Suggest(5, "m")))
+func TestStaticSuggest(t *testing.T) {
 
-}
+	Convey("Given static suggester", t, func() {
+		suggestions := []string{
+			"log", "logger", "loggest", "a", "an", "and", "aback"}
+		engine := suggest.NewSuggester(suggestions...)
 
-func TestNilEngine(t *testing.T) {
-	engine := suggest.NewSuggester()
-	assertValueEquals(t, 0, len(engine.Suggest(0, "")))
-	assertValueEquals(t, 0, len(engine.Suggest(0, "a")))
-	assertValueEquals(t, 0, len(engine.Suggest(2, "a")))
-}
+		Convey("Defensive copy of suggestions made", func() {
+			suggestions[0] = "wrong"
+			So(
+				engine.Suggest(0, ""),
+				ShouldResemble,
+				[]string{
+					"log", "logger", "loggest", "a", "an", "and", "aback"})
+		})
 
-func assertValueEquals(
-	t *testing.T, expected, actual interface{}) bool {
-	if expected != actual {
-		t.Errorf("Expected %v, got %v", expected, actual)
-		return false
-	}
-	return true
-}
+		Convey("Suggestions in same order as original list", func() {
+			So(
+				engine.Suggest(0, "a"),
+				ShouldResemble,
+				[]string{
+					"a", "an", "and", "aback"})
+		})
 
-func assertValueDeepEquals(
-	t *testing.T, expected, actual interface{}) bool {
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("Expected %v, got %v", expected, actual)
-		return false
-	}
-	return true
+		Convey("max suggestion count honored", func() {
+			So(
+				engine.Suggest(2, "log"),
+				ShouldResemble,
+				[]string{"log", "logger"})
+		})
+
+		Convey("no match gives no results", func() {
+			So(engine.Suggest(2, "lol"), ShouldBeEmpty)
+		})
+
+	})
 }
